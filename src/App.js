0245595b4c560fd77bad622e36ac027320d4d398
Jalayana <millerja14@outlook.com>
2025-05-26 01:53:01 +0800

    Updated the CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import WeatherSearch from "./WeatherSearch";
+import "./WeatherSearch.css";
 
 function App() {
   return (
@@ -7,17 +8,17 @@ function App() {
       <h1>Weather Search Engine</h1>
       <WeatherSearch defaultCity="New York" />
       <footer>
-        This project was coded by
+        This project was coded by {""}
         <a href="https://github.com/Jalayana" target="_blank" rel="noreferrer">
           Jalayana Miller
         </a>
-        , is open-sourced on
+        , is open-sourced on{" "}
         <a
-          href="https://github.com/Jalayana/new-weather-app"
+          href="https://github.com/Jalayana/weather-app-react"
           target="_blank"
           rel="noreferrer"
         >
-          GitHub
+          GitHub {""}
         </a>
         and hosted on Netlify
       </footer>
