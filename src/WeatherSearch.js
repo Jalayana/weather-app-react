266fa6ee9ad35f0fe3b16b452885ba034a34ac99
Jalayana <millerja14@outlook.com>
2025-05-26 01:32:38 +0800

    Added date and displaying the date

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -6,9 +6,29 @@ export default function WeatherSearch() {
   const [isWeatherReady, setIsWeatherReady] = useState(false);
   const [weather, setWeather] = useState({});
 
+  function formatDate(date) {
+    let hours = date.getHours();
+    if (hours < 10) hours = `0${hours}`;
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = `0${minutes}`;
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const day = days[date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+  }
+
   function displayWeather(response) {
     setIsWeatherReady(true);
     setWeather({
+      city: response.data.name, // ✅ Add this line
+      date: new Date(response.data.dt * 1000),
       temperature: response.data.main.temp,
       feels_like: response.data.main.feels_like,
       wind: response.data.wind.speed,
@@ -20,7 +40,7 @@ export default function WeatherSearch() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiKey = "b47fdf6445cd8b64ab889be77dbe56d4";
+    let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
@@ -44,7 +64,15 @@ export default function WeatherSearch() {
 
   if (isWeatherReady) {
     return (
-      <div>
+      <div className="weather-app-data">
+        <h1>{weather.city}</h1>
+        <div className="weather-app-date">{formatDate(weather.date)}</div>
+        <div className="weather-app-temperature">
+          <div className="weather-app-value">
+            {Math.round(weather.temperature)}
+          </div>
+          <div className="weather-app-unit">°C</div>
+        </div>
         {form}
         <ul>
           <li>Temperature: {Math.round(weather.temperature)}°C</li>
