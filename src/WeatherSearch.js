77abc7936c9b6db38fc7e0d7d6e1e358456dd90a
Jalayana <millerja14@outlook.com>
2025-05-26 02:33:35 +0800

    Updated the default city functionality

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -37,62 +37,63 @@ export default function WeatherSearch(props) {
       description: response.data.weather[0].description,
     });
   }
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  search();
+}
+
+useEffect(() => {
   function search() {
     let apiKey = "5aac6d0188c6f17d6d2bbe6591b6fef0";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
   }
-  function handleSubmit(event) {
-    event.preventDefault();
-    search();
-  }
 
-  useEffect(() => {
-    search();
-  }, []);
+  search();
+}, []);
 
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
+function updateCity(event) {
+  setCity(event.target.value);
+}
 
-  let form = (
-    <form onSubmit={handleSubmit} className="search-form" id="search-form">
-      <input
-        type="search"
-        placeholder="Enter a city"
-        required
-        className="search-form-input"
-        onChange={updateCity}
-      />
-      <input type="submit" value="Search" className="search-form-button" />
-    </form>
-  );
+let form = (
+  <form onSubmit={handleSubmit} className="search-form" id="search-form">
+    <input
+      type="search"
+      placeholder="Enter a city"
+      required
+      className="search-form-input"
+      onChange={updateCity}
+    />
+    <input type="submit" value="Search" className="search-form-button" />
+  </form>
+);
 
-  if (isWeatherReady) {
-    return (
-      <div className="weather-app-data">
-        <h1>{weather.city}</h1>
-        <div className="weather-app-date">{formatDate(weather.date)}</div>
-        <div className="weather-app-temperature">
-          <div className="weather-app-value">
-            {Math.round(weather.temperature)}
-          </div>
-          <div className="weather-app-unit">°C</div>
+if (isWeatherReady) {
+  return (
+    <div className="weather-app-data">
+      <h1>{weather.city}</h1>
+      <div className="weather-app-date">{formatDate(weather.date)}</div>
+      <div className="weather-app-temperature">
+        <div className="weather-app-value">
+          {Math.round(weather.temperature)}
         </div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {Math.round(weather.wind)}km/h</li>
-          <li>Feels like: {Math.round(weather.feels_like)}°C</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
+        <div className="weather-app-unit">°C</div>
       </div>
-    );
-  } else {
-    return form;
-  }
+      {form}
+      <ul>
+        <li>Temperature: {Math.round(weather.temperature)}°C</li>
+        <li>Description: {weather.description}</li>
+        <li>Humidity: {weather.humidity}%</li>
+        <li>Wind: {Math.round(weather.wind)}km/h</li>
+        <li>Feels like: {Math.round(weather.feels_like)}°C</li>
+        <li>
+          <img src={weather.icon} alt={weather.description} />
+        </li>
+      </ul>
+    </div>
+  );
+} else {
+  return form;
 }
